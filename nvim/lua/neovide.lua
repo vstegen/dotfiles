75641ca44b11eba9e8533c56d60d45022f602d2c
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2022-10-14 22:23:45 +0200

    nvim: update neovide config

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -1,7 +1,7 @@
 vim.g.neovide_refresh_rate = 60
 vim.g.neovide_refresh_idle = 5
-vim.g.neovide_scroll_animation_length = 0.2
+vim.g.neovide_scroll_animation_length = 0
 vim.g.neovide_remember_window_size = true
 
-vim.g.neovide_cursor_animation_length = 0.1
-vim.g.neovide_cursor_trail_size = 0.3
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_size = 0
