8afb392768748845fdab34dfcc51d88e9923eba5
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2022-10-21 19:55:03 +0200

    nvim: use updated cmp capabilities function

--- nvim/lua/lsp/utils.lua
+++ nvim/lua/lsp/utils.lua
@@ -7,7 +7,7 @@ M.generate_capabilities = function(server)
 
   local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
   if status_ok then
-    capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
   end
 
   capabilities.textDocument.completion.completionItem.snippetSupport = true
