80070a6243847d352ad55c63a326a51ce9fc6f73
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-06 22:52:08 +0100

    nvim: comment

--- nvim/lua/plugin-config/comment.lua
+++ nvim/lua/plugin-config/comment.lua
@@ -1,58 +1,32 @@
 local ok, comment = pcall(require, "Comment")
 if not ok then
-  return
+    return
+end
+
+function pre_hook(ctx)
+    -- Only calculate commentstring for tsx filetypes
+    if vim.bo.filetype == "typescriptreact" then
+        local U = require "Comment.utils"
+
+        -- Determine whether to use linewise or blockwise commentstring
+        local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
+
+        -- Determine the location where to calculate commentstring from
+        local location = nil
+        if ctx.ctype == U.ctype.blockwise then
+            location = require("ts_context_commentstring.utils").get_cursor_location()
+        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = require("ts_context_commentstring.utils").get_visual_start_location()
+        end
+
+        return require("ts_context_commentstring.internal").calculate_commentstring {
+            key = type,
+            location = location,
+        }
+    end
 end
 
 comment.setup {
-  padding = true,
-  sticky = true,
-  ---@type string|fun():string
-  ignore = "^$",
-  ---@type table
-  toggler = {
-    line = "gcc",
-    block = "gbc",
-  },
-  ---@type table
-  opleader = {
-    line = "gc",
-    block = "gb",
-  },
-  ---@type table
-  extra = {
-    above = "gcO",
-    below = "gco",
-    eol = "gcA",
-  },
-  ---@type boolean|table
-  mappings = {
-    basic = true,
-    extra = true,
-    extended = false,
-  },
-  ---@type fun(ctx: CommentCtx):string
-  pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
-    if vim.bo.filetype == "typescriptreact" then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end,
-  ---@type fun(ctx: CommentCtx)
-  post_hook = nil,
+    ignore = "^$",
+    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 }
