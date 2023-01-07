b019ed0881262302780b3fdb6e06d01b5a123a41
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-07 19:03:21 +0100

    nvim: null-ls

--- nvim/lua/plugin-config/null-ls.lua
+++ nvim/lua/plugin-config/null-ls.lua
@@ -53,33 +53,21 @@ local config = {
     },
     null_ls.builtins.formatting.prismaFmt,
     null_ls.builtins.formatting.goimports,
-    -- null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.stylua.with {
-        --[[ args = { "--stdin-filepath", get_file_name() }, ]]
-    },
+    null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.shfmt.with {
         filetypes = shfmt.filetypes,
     },
     null_ls.builtins.formatting.black.with { extra_args = { "--fast" } },
-    -- null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.clang_format,
-    -- null_ls.builtins.formatting.uncrustify,
     null_ls.builtins.formatting.cmake_format,
-    -- null_ls.builtins.formatting.nginx_beautifier,
     null_ls.builtins.formatting.sqlformat,
-    -- null_ls.builtins.formatting.buf,
-    -- null_ls.builtins.formatting.sqlfluff,
-    -- null_ls.builtins.formatting.zigfmt,
 
     -- diagnostics
-    -- null_ls.builtins.diagnostics.buf,
     null_ls.builtins.diagnostics.staticcheck,
     null_ls.builtins.diagnostics.golangci_lint.with {
         extra_args = { "-E", "revive", "-E", "unparam" },
     },
     null_ls.builtins.diagnostics.eslint, -- could use eslint_d as well
-    -- null_ls.builtins.diagnostics.stylelint,
-    -- null_ls.builtins.diagnostics.tidy,
     null_ls.builtins.diagnostics.luacheck,
     null_ls.builtins.diagnostics.flake8.with {
         extra_args = { "--select", "C,E,F,W,B,B950", "--extend-ignore", "E203,E501", "--max-line-length", "88" },
@@ -91,29 +79,17 @@ local config = {
     },
     null_ls.builtins.diagnostics.hadolint,
     null_ls.builtins.diagnostics.vint,
-    -- null_ls.builtins.diagnostics.codespell,
-    -- null_ls.builtins.diagnostics.proselint,
 
     -- code actions
     null_ls.builtins.code_actions.eslint, -- could use eslint_d as well
-    -- NOTE: needs the plugin to work: https://github.com/ThePrimeagen/refactoring.nvim
-    -- null_ls.builtins.code_actions.refactoring,
     null_ls.builtins.code_actions.shellcheck,
-    -- null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.gitsigns,
 }
 
 null_ls.setup {
     sources = config,
-    cmd = { "nvim" },
     debounce = 150,
-    debug = false,
-    default_timeout = 5000,
-    -- diagnostics_format = "#{m}",
     diagnostics_format = "[#{c}] #{m} (#{s})",
-    fallback_severity = vim.diagnostic.severity.ERROR,
-    log_level = "warn",
-    notify_format = "[null-ls] %s",
-    -- TODO: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
     on_attach = require("vstegen.lsp.utils").on_attach,
     update_in_insert = true,
 }
