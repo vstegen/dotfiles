57247f6190365bb6365566edae76a1d6a7613ed0
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-08 00:43:19 +0100

    fish: Use fd for fzf

--- fish/conf.d/vars.fish
+++ fish/conf.d/vars.fish
@@ -11,3 +11,7 @@ set -gx LDFLAGS "-L/opt/homebrew/opt/openssl@1.1/lib"
 # set -gx LDFLAGS "-L/opt/homebrew/opt/bison/lib"
 set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@1.1/include"
 set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
+
+set -gx FZF_DEFAULT_COMMAND "fd --type file --color=always"
+set -gx FZF_DEFAULT_OPTS "--ansi"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
