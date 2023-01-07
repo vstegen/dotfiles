cd77b75eb4bbcbf13332658d88fc178a1f12c3fa
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-07 21:49:25 +0100

    nvim: todo comments

--- nvim/lua/plugin-config/todo-comments.lua
+++ nvim/lua/plugin-config/todo-comments.lua
@@ -3,40 +3,20 @@ if not status_ok then
     return
 end
 
+--[[
+Keywords:
+FIX FIXME BUG FIXIT ISSUE
+TODO
+HACK
+WARN WARNING XXX
+PERF OPTIM PERFORMANCE OPTIMIZE
+NOTE INFO
+TEST TESTING PASSED FAILED
+]]
 todo.setup {
-    signs = true,
-    sign_priority = 8,
-    keywords = {
-        FIX = {
-            icon = " ", -- used for the sign, and search results
-            -- can be a hex color, or a named color
-            -- named colors definitions follow below
-            color = "error",
-            -- a set of other keywords that all map to this FIX keywords
-            alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
-            -- signs = false -- configure signs for some keywords individually
-        },
-        TODO = { icon = " ", color = "info" },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    },
-    merge_keywords = true, -- wheather to merge custom keywords with defaults
     highlight = {
-        -- highlights before the keyword (typically comment characters)
-        before = "", -- "fg", "bg", or empty
-        -- highlights of the keyword
-        -- wide is the same as bg, but also highlights the colon
         keyword = "bg", -- "fg", "bg", "wide", or empty
-        -- highlights after the keyword (TODO text)
-        after = "fg", -- "fg", "bg", or empty
-        -- pattern can be a string, or a table of regexes that will be checked
-        -- vim regex
         pattern = [[.*<(KEYWORDS)(\(.*\))?\s*:]],
-        comments_only = true, -- highlight only inside comments using treesitter
-        max_line_len = 400, -- ignore lines longer than this
-        exclude = {}, -- list of file types to exclude highlighting
     },
     -- list of named colors
     -- a list of hex colors or highlight groups
@@ -49,15 +29,6 @@ todo.setup {
         default = { O.palette.pink, O.palette.purple, "Identifier", "#7C3AED" },
     },
     search = {
-        command = "rg",
-        args = {
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-        },
-        -- don't replace the (KEYWORDS) placeholder
         pattern = [[\b(KEYWORDS)(\(.*\))?:]], -- ripgrep regex
     },
 }
