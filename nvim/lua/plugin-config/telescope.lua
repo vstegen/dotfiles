1f171f9da9c9ad7bc7ac493c20433f8e989d02e0
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-07 21:41:28 +0100

    nvim: telescope

--- nvim/lua/plugin-config/telescope.lua
+++ nvim/lua/plugin-config/telescope.lua
@@ -1,6 +1,6 @@
 local telescope_ok, telescope = pcall(require, "telescope")
 if not telescope_ok then
-  return
+    return
 end
 
 local actions = require "telescope.actions"
@@ -9,216 +9,147 @@ local action_layout = require "telescope.actions.layout"
 local _, trouble = pcall(require, "trouble.providers.telescope")
 
 local config = {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "truncate" },
-    file_ignore_patterns = { ".git/", "node_modules", "*/target/debug/*" },
-    initial_mode = "insert",
-    sorting_strategy = "descending",
-    selection_strategy = "reset",
-    scroll_strategy = "cycle",
-    layout_strategy = "horizontal",
-    layout_config = {
-      width = 0.95,
-      height = 0.85,
-      preview_cutoff = 120,
-      prompt_position = "top",
+    defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "truncate" },
+        file_ignore_patterns = { ".git/", "node_modules", "*/target/debug/*" },
+        initial_mode = "insert",
+        layout_config = {
+            width = 0.95,
+            height = 0.85,
+            preview_cutoff = 120,
+            prompt_position = "top",
 
-      -- defaults
-      bottom_pane = {
-        height = 25,
-        preview_cutoff = 120,
-        prompt_position = "top",
-      },
-      center = {
-        height = 0.4,
-        preview_cutoff = 40,
-        prompt_position = "top",
-        width = 0.5,
-      },
-      cursor = {
-        height = 0.9,
-        preview_cutoff = 40,
-        width = 0.8,
-      },
-      horizontal = {
-        height = 0.95,
-        preview_cutoff = 200,
-        prompt_position = "bottom",
-        width = 0.95,
-      },
-      vertical = {
-        height = 0.95,
-        preview_cutoff = 40,
-        prompt_position = "bottom",
-        width = 0.95,
-        preview_height = 0.2,
-        -- results_width = 0.8,
-      },
-    },
-    border = true,
-    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--hidden",
-      "--glob=!.git/",
-    },
-    set_env = { COLORTERM = "truecolor" },
-    color_devicons = true,
-    file_sorter = require("telescope.sorters").get_fzy_sorter,
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
-
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-
-        ["<CR>"] = actions.select_default,
-
-        ["<c-t>"] = trouble.open_with_trouble,
-
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<M-p>"] = action_layout.toggle_preview,
-        ["<M-m>"] = action_layout.toggle_mirror,
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-
-        ["<c-t>"] = trouble.open_with_trouble,
-
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-
-        ["<M-p>"] = action_layout.toggle_preview,
-        ["<M-m>"] = action_layout.toggle_mirror,
-      },
-    },
-  },
-  pickers = {
-    live_grep = {
-      only_sort_text = true,
-      layout_strategy = "vertical",
-    },
-    find_files = {},
-    git_files = {},
-    diagnostics = {
-      layout_strategy = "vertical",
-    },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-    },
-    --[[ fzy_native = {
-      override_generic_sorter = false,
-      override_file_sorter = true,
-    }, ]]
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
+            horizontal = {
+                height = 0.95,
+                preview_cutoff = 200,
+                width = 0.95,
+            },
+            vertical = {
+                height = 0.95,
+                width = 0.95,
+                preview_height = 0.2,
+            },
         },
-        ["n"] = {
-          -- your custom normal mode mappings
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob=!.git/",
         },
-      },
-    },
-    ["ui-select"] = {
-      --[[ require("telescope.themes").get_dropdown {
-        -- even more opts
-      }, ]]
-      require("telescope.themes").get_cursor {
-        -- even more opts
-      },
+        set_env = { COLORTERM = "truecolor" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
 
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
+                ["<C-c>"] = actions.close,
+
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+
+                ["<CR>"] = actions.select_default,
+
+                ["<c-t>"] = trouble.open_with_trouble,
+
+                ["<C-x>"] = actions.select_horizontal,
+                ["<C-v>"] = actions.select_vertical,
+
+                ["<C-u>"] = actions.preview_scrolling_up,
+                ["<C-d>"] = actions.preview_scrolling_down,
+
+                ["<M-p>"] = action_layout.toggle_preview,
+                ["<M-m>"] = action_layout.toggle_mirror,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+
+                ["<c-t>"] = trouble.open_with_trouble,
+
+                ["<C-x>"] = actions.select_horizontal,
+                ["<C-v>"] = actions.select_vertical,
+
+                ["<C-u>"] = actions.preview_scrolling_up,
+                ["<C-d>"] = actions.preview_scrolling_down,
+
+                ["gg"] = actions.move_to_top,
+                ["G"] = actions.move_to_bottom,
+
+                ["j"] = actions.move_selection_next,
+                ["k"] = actions.move_selection_previous,
+                ["H"] = actions.move_to_top,
+                ["M"] = actions.move_to_middle,
+                ["L"] = actions.move_to_bottom,
+
+                ["<M-p>"] = action_layout.toggle_preview,
+                ["<M-m>"] = action_layout.toggle_mirror,
+            },
+        },
     },
-  },
+    pickers = {
+        live_grep = {
+            only_sort_text = true,
+            layout_strategy = "vertical",
+        },
+        diagnostics = {
+            layout_strategy = "vertical",
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+        file_browser = {
+            theme = "ivy",
+            hijack_netrw = true,
+            mappings = {
+                ["i"] = {},
+                ["n"] = {},
+            },
+        },
+        ["ui-select"] = {
+            require("telescope.themes").get_cursor {},
+        },
+    },
 }
 
 telescope.setup(config)
 
 telescope.load_extension "fzf"
--- telescope.load_extension "fzy_native"
 telescope.load_extension "file_browser"
 telescope.load_extension "ui-select"
--- telescope.load_extension "frecency"
 
 local ok, _ = pcall(require, "project_nvim")
 if ok then
-  telescope.load_extension "projects"
+    telescope.load_extension "projects"
 end
 
 local persisted_ok, _ = pcall(require, "persisted")
 if persisted_ok then
-  telescope.load_extension "persisted"
+    telescope.load_extension "persisted"
 end
 
 local dap_ok, _ = pcall(require, "dap")
 if dap_ok then
-  telescope.load_extension "dap"
+    telescope.load_extension "dap"
 end
 
 local harpoon_ok, _ = pcall(require, "harpoon")
 if harpoon_ok then
-  telescope.load_extension "harpoon"
+    telescope.load_extension "harpoon"
 end
-
--- local noice_ok, _ = pcall(require, "noice")
--- if noice_ok then
---   telescope.load_extension "noice"
--- end
--- nvim/lua/vstegen/keymaps.lua
+++ nvim/lua/vstegen/keymaps.lua
@@ -285,4 +285,53 @@ Persisted:
 
 Projects:
     require'telescope'.extensions.projects.projects{}
+
+Telescope:
+    builtin.git_commits	Lists git commits with diff preview, checkout action <cr>, reset mixed <C-r>m, reset soft <C-r>s and reset hard <C-r>h
+    builtin.git_bcommits	Lists buffer's git commits with diff preview and checks them out on <cr>
+    builtin.git_branches	Lists all branches with log preview, checkout action <cr>, track action <C-t> and rebase action<C-r>
+    builtin.git_status	Lists current changes per file with diff preview and add action. (Multi-selection still WIP)
+    builtin.git_stash	Lists stash items in current repository with ability to apply them on <cr>
+
+    builtin.lsp_references	Lists LSP references for word under the cursor
+    builtin.lsp_incoming_calls	Lists LSP incoming calls for word under the cursor
+    builtin.lsp_outgoing_calls	Lists LSP outgoing calls for word under the cursor
+    builtin.lsp_document_symbols	Lists LSP document symbols in the current buffer
+    builtin.lsp_workspace_symbols	Lists LSP document symbols in the current workspace
+    builtin.lsp_dynamic_workspace_symbols	Dynamically Lists LSP for all workspace symbols
+    builtin.diagnostics	Lists Diagnostics for all open buffers or a specific buffer. Use option bufnr=0 for current buffer.
+    builtin.lsp_implementations	Goto the implementation of the word under the cursor if there's only one, otherwise show all options in Telescope
+    builtin.lsp_definitions	Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope
+    builtin.lsp_type_definitions	Goto the definition of the type of the word under the cursor, if there's only one, otherwise show all options in Telescope
+
+    builtin.buffers	Lists open buffers in current neovim instance
+    builtin.oldfiles	Lists previously open files
+    builtin.commands	Lists available plugin/user commands and runs them on <cr>
+    builtin.tags	Lists tags in current directory with tag location file preview (users are required to run ctags -R to generate tags or update when introducing new changes)
+    builtin.command_history	Lists commands that were executed recently, and reruns them on <cr>
+    builtin.search_history	Lists searches that were executed recently, and reruns them on <cr>
+    builtin.help_tags	Lists available help tags and opens a new window with the relevant help info on <cr>
+    builtin.man_pages	Lists manpage entries, opens them in a help window on <cr>
+    builtin.marks	Lists vim marks and their value
+    builtin.colorscheme	Lists available colorschemes and applies them on <cr>
+    builtin.quickfix	Lists items in the quickfix list
+    builtin.quickfixhistory	Lists all quickfix lists in your history and open them with builtin.quickfix or quickfix window
+    builtin.loclist	Lists items from the current window's location list
+    builtin.jumplist	Lists Jump List entries
+    builtin.vim_options	Lists vim options, allows you to edit the current value on <cr>
+    builtin.registers	Lists vim registers, pastes the contents of the register on <cr>
+    builtin.autocommands	Lists vim autocommands and goes to their declaration on <cr>
+    builtin.spell_suggest	Lists spelling suggestions for the current word under the cursor, replaces word with selected suggestion on <cr>
+    builtin.keymaps	Lists normal mode keymappings
+    builtin.filetypes	Lists all available filetypes
+    builtin.highlights	Lists all available highlights
+    builtin.current_buffer_fuzzy_find	Live fuzzy search inside of the currently open buffer
+    builtin.current_buffer_tags	Lists all of the tags for the currently open buffer, with a preview
+    builtin.resume	Lists the results incl. multi-selections of the previous picker
+    builtin.pickers	Lists the previous pickers incl. multi-selections (see :h telescope.defaults.cache_picker)
+
+    builtin.find_files	Lists files in your current working directory, respects .gitignore
+    builtin.git_files	Fuzzy search through the output of git ls-files command, respects .gitignore
+    builtin.grep_string	Searches for the string under your cursor in your current working directory
+    builtin.live_grep	Search for a string in your current working directory and get results live as you type, respects .gitignore. (Requires ripgrep)
 ]]
