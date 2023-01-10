59ab9bcaf76aaad5823d17b324cbfe48f2cc97dd
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-10 13:38:30 +0100

    nvim: Update autocmds

--- nvim/lua/vstegen/autocmds.lua
+++ nvim/lua/vstegen/autocmds.lua
@@ -16,8 +16,25 @@ local commands = {
         "FileType",
         {
             group = "_buffer_mappings",
-            pattern = { "qf", "help", "man", "floaterm", "lspinfo", "lsp-installer", "null-ls-info" },
-            command = "nnoremap <silent> <buffer> q :close<CR>",
+            pattern = {
+                "qf",
+                "help",
+                "man",
+                "floaterm",
+                "lspinfo",
+                "lsp-installer",
+                "null-ls-info",
+                "notify",
+                "lspinfo",
+                "spectre_panel",
+                "startuptime",
+                "tsplayground",
+                "PlenaryTestPopup",
+            },
+            callback = function(event)
+                vim.bo[event.buf].buflisted = false
+                vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+            end,
         },
     },
     {
@@ -41,7 +58,30 @@ local commands = {
         {
             group = "_filetype_settings",
             pattern = { "gitcommit", "markdown" },
-            command = "setlocal wrap spell",
+            callback = function()
+                vim.opt_local.wrap = true
+                vim.opt_local.spell = true
+            end,
+        },
+    },
+    {
+        { "FocusGained", "TermClose", "TermLeave" },
+        {
+            group = "_reload_file",
+            command = "checktime",
+        },
+    },
+    {
+        "BufReadPost",
+        {
+            group = "_jump_to_last_location",
+            callback = function()
+                local mark = vim.api.nvim_buf_get_mark(0, '"')
+                local lcount = vim.api.nvim_buf_line_count(0)
+                if mark[1] > 0 and mark[1] <= lcount then
+                    pcall(vim.api.nvim_win_set_cursor, 0, mark)
+                end
+            end,
         },
     },
 }
