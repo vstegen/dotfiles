ea82d30c32401299b7355f69b06269c9c4af6b4d
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-01-31 22:31:34 +0100

    nvim: Enable wrap

--- nvim/lua/vstegen/set.lua
+++ nvim/lua/vstegen/set.lua
@@ -57,7 +57,7 @@ local options = {
     numberwidth = 2, -- set number column width to 2 {default 4}
     signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 
-    wrap = false, -- display lines as one long line
+    wrap = true, -- display lines as one long line
 
     spell = false,
     spelllang = "en",
