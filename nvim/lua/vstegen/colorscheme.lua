6fad7c3a8953b483f07e9459a4b5d97a24d49e34
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-04-22 13:27:46 +0200

    nvim: Formatting

--- nvim/lua/vstegen/colorscheme.lua
+++ nvim/lua/vstegen/colorscheme.lua
@@ -20,41 +20,45 @@ local themes = {
 
         theme.setup {
             theme = "wave",
+            background = {
+                dark = "wave",
+                light = "lotus",
+            },
             transparent = false, -- do not set background color
             dimInactive = false, -- dim inactive window `:h hl-NormalNC`
             globalStatus = true, -- adjust window separators highlight for laststatus=3
-            overrides = function(colors) 
+            overrides = function(colors)
                 local c = colors.theme
-                    return {
-                        NormalFloat = { bg = "none" },
-                        FloatBorder = { bg = "none" },
-                        FloatTitle = { bg = "none" },
+                return {
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
 
-                        -- Save an hlgroup with dark background and dimmed foreground
-                        -- so that you can use it where your still want darker windows.
-                        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                        NormalDark = { fg = c.ui.fg_dim, bg = c.ui.bg_m3 },
+                    -- Save an hlgroup with dark background and dimmed foreground
+                    -- so that you can use it where your still want darker windows.
+                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                    NormalDark = { fg = c.ui.fg_dim, bg = c.ui.bg_m3 },
 
-                        -- Popular plugins that open floats will link to NormalFloat by default;
-                        -- set their background accordingly if you wish to keep them dark and borderless
-                        LazyNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
-                        MasonNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
+                    -- Popular plugins that open floats will link to NormalFloat by default;
+                    -- set their background accordingly if you wish to keep them dark and borderless
+                    LazyNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
+                    MasonNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
 
-                        -- borderless telescope
-                        TelescopeTitle = { fg = c.ui.special, bold = true },
-                        TelescopePromptNormal = { bg = c.ui.bg_p1 },
-                        TelescopePromptBorder = { fg = c.ui.bg_p1, bg = c.ui.bg_p1 },
-                        TelescopeResultsNormal = { fg = c.ui.fg_dim, bg = c.ui.bg_m1 },
-                        TelescopeResultsBorder = { fg = c.ui.bg_m1, bg = c.ui.bg_m1 },
-                        TelescopePreviewNormal = { bg = c.ui.bg_dim },
-                        TelescopePreviewBorder = { bg = c.ui.bg_dim, fg = c.ui.bg_dim },
+                    -- borderless telescope
+                    TelescopeTitle = { fg = c.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = c.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = c.ui.bg_p1, bg = c.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = c.ui.fg_dim, bg = c.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = c.ui.bg_m1, bg = c.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = c.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = c.ui.bg_dim, fg = c.ui.bg_dim },
 
-                        -- dark completion popup menu
-                        Pmenu = { fg = c.ui.shade0, bg = c.ui.bg_p1 },
-                        PmenuSel = { fg = "NONE", bg = c.ui.bg_p2 },
-                        PmenuSbar = { bg = c.ui.bg_m1 },
-                        PmenuThumb = { bg = c.ui.bg_p2 },
-                    }
+                    -- dark completion popup menu
+                    Pmenu = { fg = c.ui.shade0, bg = c.ui.bg_p1 },
+                    PmenuSel = { fg = "NONE", bg = c.ui.bg_p2 },
+                    PmenuSbar = { bg = c.ui.bg_m1 },
+                    PmenuThumb = { bg = c.ui.bg_p2 },
+                }
                 -- local prompt = c.sumiInk2
                 -- local bg = c.sumiInk0
                 -- local fg = c.fujiWhite
@@ -90,7 +94,6 @@ local themes = {
             end,
         }
         vim.cmd "colorscheme kanagawa"
-
 
         O.palette = require("kanagawa.colors").setup()
         local color_overrides = {
