b7de6e92c2b316acfa6b9036d19160df0ee436ee
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-04-22 13:22:33 +0200

    nvim: Update plugins

--- nvim/lua/vstegen/colorscheme.lua
+++ nvim/lua/vstegen/colorscheme.lua
@@ -18,53 +18,87 @@ local themes = {
             return
         end
 
-        local c = require("kanagawa.colors").setup()
-        local prompt = c.sumiInk2
-        local bg = c.sumiInk0
-        local fg = c.fujiWhite
         theme.setup {
+            theme = "wave",
             transparent = false, -- do not set background color
             dimInactive = false, -- dim inactive window `:h hl-NormalNC`
             globalStatus = true, -- adjust window separators highlight for laststatus=3
-            overrides = {
-                TelescopeNormal = {
-                    bg = bg,
-                    fg = fg,
-                },
-                TelescopeBorder = {
-                    bg = bg,
-                    fg = bg,
-                },
-                TelescopePromptNormal = {
-                    bg = prompt,
-                },
-                TelescopePromptBorder = {
-                    bg = prompt,
-                    fg = prompt,
-                },
-                TelescopePromptTitle = {
-                    bg = prompt,
-                    fg = prompt,
-                },
-                TelescopePreviewTitle = {
-                    bg = bg,
-                    fg = bg,
-                },
-                TelescopeResultsTitle = {
-                    bg = bg,
-                    fg = bg,
-                },
-            },
+            overrides = function(colors) 
+                local c = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
+
+                        -- Save an hlgroup with dark background and dimmed foreground
+                        -- so that you can use it where your still want darker windows.
+                        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                        NormalDark = { fg = c.ui.fg_dim, bg = c.ui.bg_m3 },
+
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- set their background accordingly if you wish to keep them dark and borderless
+                        LazyNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
+                        MasonNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
+
+                        -- borderless telescope
+                        TelescopeTitle = { fg = c.ui.special, bold = true },
+                        TelescopePromptNormal = { bg = c.ui.bg_p1 },
+                        TelescopePromptBorder = { fg = c.ui.bg_p1, bg = c.ui.bg_p1 },
+                        TelescopeResultsNormal = { fg = c.ui.fg_dim, bg = c.ui.bg_m1 },
+                        TelescopeResultsBorder = { fg = c.ui.bg_m1, bg = c.ui.bg_m1 },
+                        TelescopePreviewNormal = { bg = c.ui.bg_dim },
+                        TelescopePreviewBorder = { bg = c.ui.bg_dim, fg = c.ui.bg_dim },
+
+                        -- dark completion popup menu
+                        Pmenu = { fg = c.ui.shade0, bg = c.ui.bg_p1 },
+                        PmenuSel = { fg = "NONE", bg = c.ui.bg_p2 },
+                        PmenuSbar = { bg = c.ui.bg_m1 },
+                        PmenuThumb = { bg = c.ui.bg_p2 },
+                    }
+                -- local prompt = c.sumiInk2
+                -- local bg = c.sumiInk0
+                -- local fg = c.fujiWhite
+                -- return {
+                --     TelescopeNormal = {
+                --         bg = bg,
+                --         fg = fg,
+                --     },
+                --     TelescopeBorder = {
+                --         bg = bg,
+                --         fg = bg,
+                --     },
+                --     TelescopePromptNormal = {
+                --         bg = prompt,
+                --     },
+                --     TelescopePromptBorder = {
+                --         bg = prompt,
+                --         fg = prompt,
+                --     },
+                --     TelescopePromptTitle = {
+                --         bg = prompt,
+                --         fg = prompt,
+                --     },
+                --     TelescopePreviewTitle = {
+                --         bg = bg,
+                --         fg = bg,
+                --     },
+                --     TelescopeResultsTitle = {
+                --         bg = bg,
+                --         fg = bg,
+                --     },
+                -- }
+            end,
         }
         vim.cmd "colorscheme kanagawa"
 
-        O.palette = c
+
+        O.palette = require("kanagawa.colors").setup()
         local color_overrides = {
-            red = O.palette.diag.error,
-            yellow = O.palette.diag.warning,
-            blue = O.palette.diag.info,
-            green = O.palette.diag.hint,
-            pink = O.palette.sakuraPink,
+            red = O.palette.theme.diag.error,
+            yellow = O.palette.theme.diag.warning,
+            blue = O.palette.theme.diag.info,
+            green = O.palette.theme.diag.hint,
+            pink = O.palette.theme.sakuraPink,
         }
 
         vim.tbl_deep_extend("force", color_overrides, O.palette)
