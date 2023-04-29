c7711c602bc6158575d4f08a8c89dd730d620a8d
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-04-29 15:04:43 +0200

    nvim: Update dap lldb executable

--- nvim/lua/plugin-config/dap/adapters.lua
+++ nvim/lua/plugin-config/dap/adapters.lua
@@ -1,44 +1,44 @@
 local dap = require "dap"
 
 dap.adapters.lldb = {
-  type = "executable",
-  -- command = "/usr/bin/lldb",
-  command = "/opt/homebrew/Cellar/llvm/14.0.6_1/bin/lldb-vscode",
-  name = "lldb",
+    type = "executable",
+    -- command = "/usr/bin/lldb",
+    command = "/opt/homebrew/Cellar/llvm/16.0.2/bin/lldb-vscode",
+    name = "lldb",
 }
 
 dap.adapters.rt_lldb = {
-  type = "executable",
-  -- command = "/usr/bin/lldb",
-  command = "/opt/homebrew/Cellar/llvm/14.0.6_1/bin/lldb-vscode",
-  name = "rt_lldb",
+    type = "executable",
+    -- command = "/usr/bin/lldb",
+    command = "/opt/homebrew/Cellar/llvm/16.0.2/bin/lldb-vscode",
+    name = "rt_lldb",
 }
 
 dap.configurations.cpp = {
-  {
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {},
+    {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
 
-    -- 💀
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
-  },
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+    },
 }
 
 dap.configurations.c = dap.configurations.cpp
@@ -46,5 +46,5 @@ dap.configurations.rust = dap.configurations.cpp
 
 local dap_go_ok, dap_go = pcall(require, "dap-go")
 if dap_go_ok then
-  dap_go.setup()
+    dap_go.setup()
 end
