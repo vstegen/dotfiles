f2dd1941f666f5a5bbe5203dfcb7e686e0248673
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-05-01 20:53:40 +0200

    nvim: Fix bug when looking for hidden files with telescope

--- nvim/lua/plugin-config/which-key.lua
+++ nvim/lua/plugin-config/which-key.lua
@@ -302,7 +302,6 @@ local n_mappings = {
                 require("telescope.builtin").find_files {
                     hidden = true,
                     prompt_title = "Find Hidden Files",
-                    file_ignore_patterns = { "^./.git/" },
                 }
             end,
             "Find (hidden) file",
@@ -836,7 +835,6 @@ local n_mappings = {
                 require("telescope.builtin").find_files {
                     hidden = true,
                     prompt_title = "Find Hidden Files",
-                    file_ignore_patterns = { "^./.git/" },
                 }
             end,
             "Find (hidden) file",
