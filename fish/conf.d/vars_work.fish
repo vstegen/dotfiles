832d7d7d7053aefd085ba7f4369160a0e423dc57
Marvin van Stegen <stegenm@amazon.com>
2023-05-02 08:39:37 +0000

    fish: Add work env vars

---
+++ fish/conf.d/vars_work.fish
@@ -0,0 +1,18 @@
+set -Ux BRAZIL_WORKSPACE_DEFAULT_LAYOUT short
+
+set -Ux AUTO_TITLE_SCREENS "NO"
+
+set -Ux RPROMPT 
+
+set -Ux DEV_DESKTOP "dev-dsk-stegenm-1b-9b92337e.eu-west-1.amazon.com"
+
+# fixes coloring issue for tmux and nvim in alacritty
+set -Ux TERM screen-256color
+
+set -Ux GOPROXY direct
+set -Ux GOPATH $HOME/go
+
+set -Ux DEV_ACCOUNT_2 "702660373025"
+set -Ux DEV_ACCOUNT "990453573269"
+
+set -Ux MOONTIDE_SCRIPTS ~/Moontide/Commons/src/MoontideScripts/
