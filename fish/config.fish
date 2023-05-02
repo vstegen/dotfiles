b8fc5a4dd238e310b9fcf1e77b4aadaddc2ab66e
Marvin van Stegen <stegenm@amazon.com>
2023-05-02 10:48:18 +0200

    fish: Update path

--- fish/config.fish
+++ fish/config.fish
@@ -19,4 +19,4 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # fish_add_path $HOME/.emacs.d/bin /opt/homebrew/opt/openssl@1.1/bin /opt/homebrew/opt/bison/bin $HOME/.bun/bin $HOME/.local/bin $HOME/bin
 
-fish_add_path $HOME/.local/bin $HOME/bin /apollo/env/envImprovement/bin $HOME/.bun/bin $HOME/.toolbox/bin /usr/local/go/bin $HOME/.local/bin $GOPATH/bin $HOME/squashfs-root/usr/bin $HOME/.rbenv/bin $HOME/.fzf/bin
+fish_add_path $HOME/.local/bin $HOME/bin /apollo/env/envImprovement/bin $HOME/.bun/bin $HOME/.toolbox/bin /usr/local/go/bin $HOME/.local/bin $GOPATH/bin $HOME/squashfs-root/usr/bin $HOME/.rbenv/bin $HOME/.fzf/bin $HOME/.cargo/bin $HOME/.emacs.d/bin
