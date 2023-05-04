df7495dfec4b992a77e2dfb21a082f8a72105855
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-05-04 21:58:51 +0200

    nvim: After dap adapter configs for C++ and Rust

--- nvim/lua/plugin-config/dap/adapters.lua
+++ nvim/lua/plugin-config/dap/adapters.lua
@@ -2,16 +2,8 @@ local dap = require "dap"
 
 dap.adapters.lldb = {
     type = "executable",
-    -- command = "/usr/bin/lldb",
-    command = "/opt/homebrew/Cellar/llvm/16.0.2/bin/lldb-vscode",
+    command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
     name = "lldb",
-}
-
-dap.adapters.rt_lldb = {
-    type = "executable",
-    -- command = "/usr/bin/lldb",
-    command = "/opt/homebrew/Cellar/llvm/16.0.2/bin/lldb-vscode",
-    name = "rt_lldb",
 }
 
 dap.configurations.cpp = {
@@ -25,24 +17,57 @@ dap.configurations.cpp = {
         cwd = "${workspaceFolder}",
         stopOnEntry = false,
         args = {},
-
-        -- 💀
-        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-        --
-        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-        --
-        -- Otherwise you might get the following error:
-        --
-        --    Error on launch: Failed to attach to the target process
-        --
-        -- But you should be aware of the implications:
-        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-        -- runInTerminal = false,
+        env = function()
+            local variables = {}
+            for k, v in pairs(vim.fn.environ()) do
+                table.insert(variables, string.format("%s=%s", k, v))
+            end
+            return variables
+        end,
     },
 }
 
 dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+
+dap.configurations.rust = {
+    {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+        initCommands = function()
+            -- Find out where to look for the pretty printer Python module
+            local rustc_sysroot = vim.fn.trim(vim.fn.system "rustc --print sysroot")
+
+            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+            local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
+
+            local commands = {}
+            local file = io.open(commands_file, "r")
+            if file then
+                for line in file:lines() do
+                    table.insert(commands, line)
+                end
+                file:close()
+            end
+            table.insert(commands, 1, script_import)
+
+            return commands
+        end,
+        env = function()
+            local variables = {}
+            for k, v in pairs(vim.fn.environ()) do
+                table.insert(variables, string.format("%s=%s", k, v))
+            end
+            return variables
+        end,
+    },
+}
 
 local dap_go_ok, dap_go = pcall(require, "dap-go")
 if dap_go_ok then
