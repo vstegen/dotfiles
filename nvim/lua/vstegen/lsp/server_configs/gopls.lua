f2c762383fca1e988c22af49c422f44f015b9c17
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-05-04 22:21:34 +0200

    nvim: Add more analyses to go LSP

--- nvim/lua/vstegen/lsp/server_configs/gopls.lua
+++ nvim/lua/vstegen/lsp/server_configs/gopls.lua
@@ -1,15 +1,17 @@
 return {
-  root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
-  settings = {
-    gopls = {
-      experimentalPostfixCompletions = true,
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
-      codelenses = { test = true },
-      gofumpt = true,
+    root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
+    settings = {
+        gopls = {
+            experimentalPostfixCompletions = true,
+            analyses = {
+                nilness = true,
+                shadow = true,
+                unusedparams = true,
+                shadow = true,
+            },
+            staticcheck = true,
+            codelenses = { test = true },
+            gofumpt = true,
+        },
     },
-  },
 }
