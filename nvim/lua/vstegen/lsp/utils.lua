02bdb530f8a35dd1f9a48861004f20887e2db080
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-05-08 19:48:49 +0200

    nvim: Add lsp capabilities

--- nvim/lua/vstegen/lsp/utils.lua
+++ nvim/lua/vstegen/lsp/utils.lua
@@ -10,6 +10,14 @@ M.generate_capabilities = function()
 
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+    capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
+    capabilities.textDocument.completion.completionItem.preselectSupport = true
+    capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+    capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+    capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+    capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+    capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+
     capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = {
             "documentation",
