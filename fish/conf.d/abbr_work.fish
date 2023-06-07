400a56b7c76d049df6889af870b3348f4d6c92b8
Marvin van Stegen <stegenm@amazon.com>
2023-06-07 10:53:02 +0200

    fish: Add mwinit abbr

--- fish/conf.d/abbr_work.fish
+++ fish/conf.d/abbr_work.fish
@@ -4,6 +4,8 @@ abbr -ag gcom "git checkout mainline"
 # directly work related
 abbr -ag login 'kinit -f && mwinit --aea'
 
+abbr -ag mw 'mwinit --aea'
+
 abbr -a bb brazil-build
 abbr -a br brazil-build release
 abbr -a bre brazil-runtime-exec
