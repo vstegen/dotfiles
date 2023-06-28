24bca7b0439e7d33187423fcf2c08dafab32f359
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-06-28 10:34:34 +0200

    nvim: Update luals config

--- nvim/lua/vstegen/lsp/server_configs/lua_ls.lua
+++ nvim/lua/vstegen/lsp/server_configs/lua_ls.lua
@@ -9,27 +9,18 @@ end
 return {
     settings = {
         Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-                -- Setup your lua path
-                path = get_lua_runtime_path(),
-            },
+            -- runtime = {
+            --     -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            --     version = "LuaJIT",
+            --     -- Setup your lua path
+            --     path = get_lua_runtime_path(),
+            -- },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = { "vim", "O" },
+                globals = { "vim" },
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
-                -- library = {
-                --   [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                --   [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                --   [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
-                -- },
                 library = vim.api.nvim_get_runtime_file("", true),
-                maxPreload = 100000,
-                preloadFileSize = 10000,
-                checkThirdParty = false,
             },
             telemetry = {
                 enable = false,
