e584024113c5e2bcf4c44a737a72044ae1ab9511
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-07-03 15:18:45 +0200

    wez: Set font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,12 @@
+local wezterm = require("wezterm")
+
 local config = {
 	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font({
+		family = "MonoLisa Custom",
+		weight = "Bold",
+	}),
+	font_size = 14.0,
 }
 
 return config
