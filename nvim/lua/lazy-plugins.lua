4c289d1b38538a5c7efefeb854ccd4ec640bae44
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-07-05 21:46:19 +0200

    nvim: Disable foldcolumn

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -1021,7 +1021,7 @@ require("lazy").setup({
         "kevinhwang91/nvim-ufo",
         dependencies = { "kevinhwang91/promise-async" },
         init = function()
-            vim.o.foldcolumn = "1" -- "0" is also fine
+            vim.o.foldcolumn = "0" -- 0 and 1 are fine
             vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
             vim.o.foldlevelstart = 99
             vim.o.foldenable = true
