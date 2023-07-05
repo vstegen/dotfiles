c5ebe8d467cdb80391f3eb160ee9fd8262eab85c
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-07-05 17:58:14 +0200

    nvim: Move LSP signature to lazy config

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -37,9 +37,16 @@ require("lazy").setup({
     },
     {
         "ray-x/lsp_signature.nvim",
-        config = function()
-            require "plugin-config.lsp_signature"
-        end,
+        opts = {
+            noice = false, -- true if using noice to render markdown
+            floating_window = false,
+            hint_enable = false, -- disable virtual text
+            doc_lines = 0, -- do not show docs
+            handler_opts = {
+                border = "single",
+            },
+            toggle_key = "<M-x>",
+        },
         enabled = true,
     },
     {
