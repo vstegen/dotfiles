e88bc5edca08886e6bdf47b88bf092286614c73b
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-07-16 12:50:13 +0200

    nvim: Clean up old config

--- nvim/lua/vstegen/neovide.lua
+++
@@ -1,7 +0,0 @@
-vim.g.neovide_refresh_rate = 60
-vim.g.neovide_refresh_idle = 5
-vim.g.neovide_scroll_animation_length = 0
-vim.g.neovide_remember_window_size = true
-
-vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_cursor_trail_size = 0
