998f0a3d4b8fd70ef8bcb4203b1cbe598d6a50c8
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-07-16 12:35:45 +0200

    nvim: Copy colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,10 +8,8 @@ require "vstegen2.globals"
 
 require "vstegen2.config.keymaps"
 
-require "vstegen.colorscheme"
+require "vstegen2.colorscheme"
 
 require "vstegen2.config.autocmds"
 
 require "vstegen.lsp"
-
-require "vstegen.neovide"
