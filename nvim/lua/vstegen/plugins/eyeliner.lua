3aa69e371e01246a543b1a9a6f69c69f4452aa63
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-08-08 21:03:44 +0200

    nvim: Add eyeliner.nvim

---
+++ nvim/lua/vstegen/plugins/eyeliner.lua
@@ -0,0 +1,7 @@
+return {
+    "jinh0/eyeliner.nvim",
+    opts = {
+        highlight_on_key = true, -- show highlights only after keypress
+        dim = false, -- dim all other characters if set to true (recommended!)
+    },
+}
