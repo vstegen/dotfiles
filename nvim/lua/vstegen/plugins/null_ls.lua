bcc4cc2a67a392208f5ddb5818f0ec07ba320e59
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-08-08 21:13:57 +0200

    nvim(work): Add global prettierrc config

--- nvim/lua/vstegen/plugins/null_ls.lua
+++ nvim/lua/vstegen/plugins/null_ls.lua
@@ -8,14 +8,16 @@ return {
             { "jose-elias-alvarez/typescript.nvim" }, -- only used for the code actions
         },
         config = function()
-            local nls = require("null-ls")
+            local nls = require "null-ls"
             local builtins = nls.builtins
 
-            nls.setup({
+            nls.setup {
                 root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
                 sources = {
                     -- formatting
-                    builtins.formatting.prettierd,
+                    builtins.formatting.prettierd.with {
+                        extra_args = { "--config", vim.fn.expand "$HOME/.prettierrc" },
+                    },
                     builtins.formatting.stylua,
                     builtins.formatting.shfmt,
                     builtins.formatting.goimports,
@@ -24,9 +26,9 @@ return {
 
                     -- diagnostics
                     builtins.diagnostics.staticcheck,
-                    builtins.diagnostics.golangci_lint.with({
+                    builtins.diagnostics.golangci_lint.with {
                         extra_args = { "-E", "revive", "-E", "unparam" },
-                    }),
+                    },
                     builtins.diagnostics.eslint_d,
                     builtins.diagnostics.luacheck,
                     builtins.diagnostics.markdownlint,
@@ -38,11 +40,11 @@ return {
                     builtins.code_actions.shellcheck,
                     builtins.code_actions.gomodifytags,
                     builtins.code_actions.impl,
-                    require("typescript.extensions.null-ls.code-actions"),
+                    require "typescript.extensions.null-ls.code-actions",
                 },
                 diagnostics_format = "[#{c}] #{m} (#{s})",
-                on_attach = require("vstegen.lsp.utils").default_on_attach
-            })
+                on_attach = require("vstegen.lsp.utils").default_on_attach,
+            }
         end,
     },
 }
