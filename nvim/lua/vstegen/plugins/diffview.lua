870acf2419f514aac09fccce0ebb1f734ad1e5e0
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-08-08 21:04:28 +0200

    nvim: Fix diffview merge layout

--- nvim/lua/vstegen/plugins/diffview.lua
+++ nvim/lua/vstegen/plugins/diffview.lua
@@ -2,6 +2,12 @@ return {
     {
         "sindrets/diffview.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = true,
+        opts = {
+            view = {
+                merge_tool = {
+                    layout = "diff3_mixed",
+                },
+            },
+        },
     },
 }
