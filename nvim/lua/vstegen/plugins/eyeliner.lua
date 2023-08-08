6dabf725cd663464cbf11166a5f0113db2e25f22
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-08-08 23:12:49 +0200

    nvim: Disable eyeliner

--- nvim/lua/vstegen/plugins/eyeliner.lua
+++ nvim/lua/vstegen/plugins/eyeliner.lua
@@ -2,6 +2,7 @@ return {
     "jinh0/eyeliner.nvim",
     opts = {
         highlight_on_key = true, -- show highlights only after keypress
-        dim = false, -- dim all other characters if set to true (recommended!)
+        dim = true, -- dim all other characters if set to true (recommended!)
     },
+    enabled = false,
 }
