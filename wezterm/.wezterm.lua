523e795c0d1d3dffb1136ce1a07e2a3ec2e50c74
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-09-05 21:29:34 +0200

    wezterm: Reduce font weight

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font({ family = "MonoLisaCustom Nerd Font Mono", weight = "Bold" })
+config.font = wezterm.font({ family = "MonoLisaCustom Nerd Font Mono", weight = "Medium" })
 config.font_size = 14.0
 config.color_scheme = "Catppuccin Mocha"
 
