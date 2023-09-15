566a72fca2ef5a255061611986ba233332b2e5b2
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-09-15 21:40:09 +0200

    wezterm: Use Pragmata Pro

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,8 +5,8 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font({ family = "MonoLisaCustom Nerd Font Mono", weight = "Medium" })
-config.font_size = 14.0
+config.font = wezterm.font({ family = "PragmataPro Mono" })
+config.font_size = 15.0
 config.color_scheme = "Catppuccin Mocha"
 
 return config
