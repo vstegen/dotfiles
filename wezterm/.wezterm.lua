a4c5e37eb9fe8e452e91ac9845282eef6a1e98f0
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-12-30 12:12:34 +0900

    wezterm: Add tab bar config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,5 +9,8 @@ config.font = wezterm.font({ family = "PragmataPro Mono" })
 config.font_size = 15.0
 config.color_scheme = "Catppuccin Mocha"
 config.line_height = 1.1
+-- config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
