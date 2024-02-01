432032c73d3d61c7429ef65f75d5672a0cb38ec4
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2024-02-01 11:33:53 +0900

    nvim: Add autocmd for detecting non-utf8 files

--- nvim_new/lua/vstegen/autocmds.lua
+++ nvim_new/lua/vstegen/autocmds.lua
@@ -90,7 +90,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 -- Fix conceallevel for json & help files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "json", "jsonc" },
+    pattern = { "json", "jsonc", "markdown" },
     callback = function()
         vim.wo.spell = false
         vim.wo.conceallevel = 0
@@ -109,6 +109,19 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 })
 
 vim.api.nvim_create_autocmd("Filetype", { pattern = "rust", command = "set colorcolumn=100" })
+
+-- Display a message when the current file is not in utf-8 format.
+-- Note that we need to use `unsilent` command here because of this issue:
+-- https://github.com/vim/vim/issues/4379
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+    pattern = "*",
+    group = augroup "non_utf8_file",
+    callback = function()
+        if vim.bo.fileencoding ~= "utf-8" then
+            vim.notify("File not in UTF-8 format!", vim.log.levels.WARN, { title = "nvim-config" })
+        end
+    end,
+})
 
 vim.api.nvim_create_user_command("ClearReg", function()
     print "Clearing registers"
