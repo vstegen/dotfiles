355e381f95c74ee6a5be43141bc06c518f874588
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2024-03-22 00:17:00 +0900

    fish: Use neovim for man pages

--- fish/conf.d/vars.fish
+++ fish/conf.d/vars.fish
@@ -1,6 +1,8 @@
 set -Ux HOMEBREW_NO_AUTO_UPDATE 3
 
 set -Ux EDITOR nvim
+# man pages use neovim as the pager
+set -Ux MANPAGER "nvim +Man!"
 
 set -Ux FIREFOX_PROFILE "/Users/marvin/Library/Application Support/Firefox/Profiles/y2wbzv7c.default-release"
 
@@ -18,7 +20,7 @@ set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@1.1/include"
 set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
 
 set -gx FZF_DEFAULT_COMMAND "fd --type file --color=always"
-set -gx FZF_DEFAULT_OPTS "--ansi"
+set -gx FZF_DEFAULT_OPTS --ansi
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
 # postgresql
