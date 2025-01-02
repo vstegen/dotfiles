6230d2a4c717a7a652786e186367ae3fa4f00bc9
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-01-02 07:09:03 +0100

    nvim: Add cmp config back

--- nvim/lua/vstegen/lsp.lua
+++ nvim/lua/vstegen/lsp.lua
@@ -2,12 +2,13 @@ local M = {}
 
 function M.default_capabilities()
     local has_blink, blink_nvim_lsp = pcall(require, "blink.cmp")
+    local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 
     local capabilities = vim.tbl_deep_extend(
         "force",
         {},
         vim.lsp.protocol.make_client_capabilities(),
-        has_blink and blink_nvim_lsp.get_lsp_capabilities() or {}
+        (has_blink and blink_nvim_lsp.get_lsp_capabilities()) or (has_cmp and cmp_nvim_lsp.default_capabilities()) or {}
     )
 
     capabilities.textDocument.completion.completionItem.snippetSupport = true
