f2a2e17bfc829621c0db4a2814bd3133e8444e33
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-03-21 12:31:04 +0900

    nvim: Add tailwindcss support for elixir files

--- nvim/lua/vstegen/lsp.lua
+++ nvim/lua/vstegen/lsp.lua
@@ -355,6 +355,13 @@ M.servers = {
         }),
     },
     tailwindcss = {
+        init_options = {
+            userLanguages = {
+                elixir = "html-eex",
+                eelixir = "html-eex",
+                heex = "html-eex",
+            },
+        },
         settings = {
             tailwindCSS = {
                 validate = true,
