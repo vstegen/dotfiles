b1464319bb12833571ebdc61473c9575a3a781b9
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-05-21 22:17:51 +0900

    nvim: Remove kanagawa theme

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -861,45 +861,6 @@ require("lazy").setup({
             vim.cmd.colorscheme "catppuccin"
         end,
     },
-    {
-        "rebelot/kanagawa.nvim",
-        priority = 1000,
-        lazy = true,
-        opts = {
-            theme = "wave",
-            background = {
-                dark = "wave",
-                light = "lotus",
-            },
-            transparent = false,
-            dimInactive = false,
-            globalStatus = true,
-            overrides = function(colors)
-                local c = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-
-                    -- Save an hlgroup with dark background and dimmed foreground
-                    -- so that you can use it where your still want darker windows.
-                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                    NormalDark = { fg = c.ui.fg_dim, bg = c.ui.bg_m3 },
-
-                    -- Popular plugins that open floats will link to NormalFloat by default;
-                    -- set their background accordingly if you wish to keep them dark and borderless
-                    LazyNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
-                    MasonNormal = { bg = c.ui.bg_m3, fg = c.ui.fg_dim },
-
-                    -- dark completion popup menu
-                    Pmenu = { fg = c.ui.shade0, bg = c.ui.bg_p1 },
-                    PmenuSel = { fg = "NONE", bg = c.ui.bg_p2 },
-                    PmenuSbar = { bg = c.ui.bg_m1 },
-                    PmenuThumb = { bg = c.ui.bg_p2 },
-                }
-            end,
-        },
-    },
     -- misc
     { "tpope/vim-sleuth" },
     {
