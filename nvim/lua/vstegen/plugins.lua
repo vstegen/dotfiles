ab521f9c3614154c42b0d23331a11a5705096bd2
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-05-21 22:09:31 +0900

    nvim: Remove deprecated handler changes

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -263,15 +263,6 @@ require("lazy").setup({
                 vim.diagnostic.config(inactive_diagnostics_config)
                 inactive_diagnostics_config = new_inactive_diagnostic_config
             end, { desc = "Toggle diagnostic virtual_lines" })
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-                border = "single",
-            })
-
-            vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-                border = "single",
-                focusable = true,
-                relative = "cursor",
-            })
         end,
     },
     {
