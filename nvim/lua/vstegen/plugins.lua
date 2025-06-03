0e1d3bafe9f1f11d23556a9c809a825bc2803a81
Marvin van Stegen <vanstegen.marvin@i.moneyforward.com>
2025-06-03 16:09:51 +0900

    nvim: Add kanagawa

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -821,6 +821,7 @@ require("lazy").setup({
         "catppuccin/nvim",
         priority = 1000,
         lazy = false,
+        enabled = false,
         name = "catppuccin",
         opts = {
             styles = {
@@ -859,6 +860,57 @@ require("lazy").setup({
         config = function(_, opts)
             require("catppuccin").setup(opts)
             vim.cmd.colorscheme "catppuccin"
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            compile = false,
+            undercurl = false,
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = false },
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = false,
+            dimInactive = false,
+            terminalColors = true,
+            theme = "wave", -- Load "wave" theme
+            background = { -- map the value of 'background' option to a theme
+                dark = "wave", -- try "dragon" !
+                light = "lotus",
+            },
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    -- transparent floats
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+
+                    -- Save an hlgroup with dark background and dimmed foreground
+                    -- so that you can use it where your still want darker windows.
+                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                    -- Popular plugins that open floats will link to NormalFloat by default;
+                    -- set their background accordingly if you wish to keep them dark and borderless
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+                    -- dark completion menu
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+                }
+            end,
+        },
+        config = function(_, opts)
+            require("kanagawa").setup(opts)
+            vim.cmd.colorscheme "kanagawa"
         end,
     },
     -- misc
