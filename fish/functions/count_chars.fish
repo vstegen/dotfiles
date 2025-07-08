250f1c711e86b59fb12701f3ddd4b7e66fa599ca
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-07-08 22:34:18 +0900

    fish: incl whitespace in count_chars

--- fish/functions/count_chars.fish
+++ fish/functions/count_chars.fish
@@ -1,3 +1,3 @@
 function count_chars -d "Count all characters in a directory"
-    find . -type f -exec cat {} \; | tr -d '\n\r\t ' | fold -w1 | sort | uniq -c | sort -nr
+    find . -type f -exec cat {} \; | fold -w1 | sort | uniq -c | sort -nr
 end
