eabc63e318f6f768962f52ce8ed9f92e67884571
Marvin van Stegen <vanstegen.marvin@i.moneyforward.com>
2025-08-04 16:48:02 +0900

    nvim: add fff.nvim

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -463,6 +463,24 @@ require("lazy").setup({
         end,
     },
     {
+        "dmtrKovalenko/fff.nvim",
+        build = "cargo build --release",
+        -- or if you are using nixos
+        -- build = "nix run .#release",
+        opts = {
+            -- pass here all the options
+        },
+        keys = {
+            {
+                "<leader>fs", -- try it if you didn't it is a banger keybinding for a picker
+                function()
+                    require("fff").find_files() -- or find_in_git_root() if you only want git files
+                end,
+                desc = "Open fff file picker",
+            },
+        },
+    },
+    {
         "ibhagwan/fzf-lua",
         dependencies = { "echasnovski/mini.icons" },
         keys = function()
