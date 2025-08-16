e8eb56f0ea8c8ccc281953f988c3e4cc3ee5c134
vstegen <github.pQcVWa7e@papertrail.fyi>
2025-08-16 23:02:30 +0900

    fish: Orbstack init

--- fish/config.fish
+++ fish/config.fish
@@ -19,3 +19,7 @@ mise activate fish | source
 jj util completion fish | source
 
 fish_add_path $HOME/.emacs.d/bin /opt/homebrew/opt/openssl@1.1/bin /opt/homebrew/opt/bison/bin $HOME/.bun/bin $HOME/.local/bin $HOME/bin /opt/homebrew/opt/postgresql@15/bin /opt/homebrew/opt/llvm/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
