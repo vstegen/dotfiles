4ac95bd8bf14f959b8c3b26d343946f1c40ddc9e
vstegen <github.pQcVWa7e@papertrail.fyi>
2025-10-25 19:55:14 +0900

    nvim: fix rust config

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -205,7 +205,6 @@ require("lazy").setup({
             })
 
             vim.lsp.config.cssls = lsp.servers.cssls
-            -- vim.lsp.config.elixirls = lsp.servers.elixirls
             vim.lsp.config.emmet_language_server = lsp.servers.emmet_language_server
             vim.lsp.config.gopls = lsp.servers.gopls
             vim.lsp.config.html = lsp.servers.html
@@ -282,33 +281,34 @@ require("lazy").setup({
         "mrcjkb/rustaceanvim",
         version = "^6", -- Recommended
         ft = { "rust" },
+        lazy = false,
         opts = {
             server = lsp.servers.rust_analyzer,
         },
         config = function(_, opts)
-            vim.g.rustaceanvim = function()
-                -- Update this path
-                local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
-                local codelldb_path = extension_path .. "adapter/codelldb"
-                local liblldb_path = extension_path .. "lldb/lib/liblldb"
-                local this_os = vim.uv.os_uname().sysname
+            local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
+            local codelldb_path = extension_path .. "adapter/codelldb"
+            local liblldb_path = extension_path .. "lldb/lib/liblldb"
+            local this_os = vim.uv.os_uname().sysname
 
-                -- The path is different on Windows
-                if this_os:find "Windows" then
-                    codelldb_path = extension_path .. "adapter\\codelldb.exe"
-                    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-                else
-                    -- The liblldb extension is .so for Linux and .dylib for MacOS
-                    liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-                end
-
-                local cfg = require "rustaceanvim.config"
-                return {
-                    dap = {
-                        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-                    },
-                }
+            if this_os:find "Windows" then
+                codelldb_path = extension_path .. "adapter\\codelldb.exe"
+                liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+            else
+                liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
             end
+
+            local cfg = require "rustaceanvim.config"
+            vim.g.rustaceanvim = {
+                dap = {
+                    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+                },
+                server = {
+                    default_settings = {
+                        ["rust-analyzer"] = opts.server.settings["rust-analyzer"],
+                    },
+                },
+            }
         end,
     },
     {
