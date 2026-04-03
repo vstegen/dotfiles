89f148e0ae95ba48d3015412e5d5914046f5078d
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-04-03 19:11:13 +0900

    nvim: use UI2

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,30 @@ if vim.env.PROF then
     }
 end
 
+require("vim._core.ui2").enable {
+    enable = true,
+    msg = {
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = "cmd",
+        cmd = {
+            height = 0.5,
+        },
+        dialog = {
+            height = 0.5,
+        },
+        msg = {
+            height = 0.5,
+            timeout = 4000,
+        },
+        pager = {
+            height = 0.5,
+        },
+    },
+}
+
 require "vstegen.options"
 require "vstegen.plugins"
 require "vstegen.globals"
