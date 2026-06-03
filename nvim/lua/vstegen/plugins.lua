1f22597d72a88c70074478883f48b96ee1ffb166
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-06-03 20:54:59 +0900

    nvim: use vim.schedule for mason

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -194,19 +194,15 @@ require("lazy").setup({
         config = function(_, opts)
             require("mason").setup(opts)
             local mr = require "mason-registry"
-            local function ensure_installed()
+
+            vim.schedule(function()
                 for _, tool in ipairs(opts.ensure_installed) do
-                    local p = mr.get_package(tool)
-                    if not p:is_installed() then
+                    local ok, p = pcall(mr.get_package, tool)
+                    if ok and not p:is_installed() then
                         p:install()
                     end
                 end
-            end
-            if mr.refresh then
-                mr.refresh(ensure_installed)
-            else
-                ensure_installed()
-            end
+            end)
         end,
     },
     {
