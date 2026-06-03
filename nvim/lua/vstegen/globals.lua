0776a9e09663b96275f1d34147b57dbbf62784d5
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-06-03 20:50:09 +0900

    nvim: lazy load globals

--- nvim/lua/vstegen/globals.lua
+++ nvim/lua/vstegen/globals.lua
@@ -1,15 +1,11 @@
-local ok, plenary_reload = pcall(require, "plenary.reload")
-local reloader = require
-if ok then
-    reloader = plenary_reload.reload_module
-end
-
 P = function(v)
     print(vim.inspect(v))
     return v
 end
 
 RELOAD = function(...)
+    local ok, plenary_reload = pcall(require, "plenary.reload")
+    local reloader = ok and plenary_reload.reload_module or require
     return reloader(...)
 end
 
