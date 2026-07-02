b9b7f4dcb6fb152fb3aa50fdd7c40dd36a81300b
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-07-02 20:45:19 +0900

    nvim: drop debugger configs

--- nvim/lua/vstegen/plugins.lua
+++ nvim/lua/vstegen/plugins.lua
@@ -145,11 +145,8 @@ require("lazy").setup({
         cmd = { "Mason", "MasonUpdate" },
         opts = {
             ensure_installed = {
-                "codelldb", -- rust debugging
                 "hadolint",
-                "js-debug-adapter", -- typescript debugging
                 "prettierd",
-                "delve",
                 "gofumpt",
                 "impl",
                 "ruff",
@@ -320,24 +317,8 @@ require("lazy").setup({
         ft = { "rust" },
         config = function(_, _)
             local lsp = require "vstegen.lsp"
-
-            local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
-            local codelldb_path = extension_path .. "adapter/codelldb"
-            local liblldb_path = extension_path .. "lldb/lib/liblldb"
-            local this_os = vim.uv.os_uname().sysname
-
-            if this_os:find "Windows" then
-                codelldb_path = extension_path .. "adapter\\codelldb.exe"
-                liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-            else
-                liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-            end
-
             local cfg = require "rustaceanvim.config"
             vim.g.rustaceanvim = {
-                dap = {
-                    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-                },
                 server = {
                     default_settings = {
                         ["rust-analyzer"] = lsp.servers.rust_analyzer.server.settings["rust-analyzer"],
