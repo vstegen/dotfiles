31241d8bf1ce48eaa1da6415d18f65e90cf50375
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-07-02 20:48:37 +0900

    nvim: remove autocmds for lsp progress

--- nvim/lua/vstegen/autocmds_lsp.lua
+++ nvim/lua/vstegen/autocmds_lsp.lua
@@ -13,26 +13,26 @@ local function set_keymaps(keymaps, buf)
     end
 end
 
-vim.api.nvim_create_autocmd("LspProgress", {
-    group = vim.api.nvim_create_augroup("vstegen_lsp_progress", {}),
-    callback = function()
-        vim.cmd.redrawstatus()
-    end,
-})
+-- vim.api.nvim_create_autocmd("LspProgress", {
+--     group = vim.api.nvim_create_augroup("vstegen_lsp_progress", {}),
+--     callback = function()
+--         vim.cmd.redrawstatus()
+--     end,
+-- })
 
-vim.api.nvim_create_autocmd("LspProgress", {
-    callback = function(ev)
-        local value = ev.data.params.value
-        vim.api.nvim_echo({ { value.message or "done" } }, false, {
-            id = "lsp." .. ev.data.client_id,
-            kind = "progress",
-            source = "vim.lsp",
-            title = value.title,
-            status = value.kind ~= "end" and "running" or "success",
-            percent = value.percentage,
-        })
-    end,
-})
+-- vim.api.nvim_create_autocmd("LspProgress", {
+--     callback = function(ev)
+--         local value = ev.data.params.value
+--         vim.api.nvim_echo({ { value.message or "done" } }, false, {
+--             id = "lsp." .. ev.data.client_id,
+--             kind = "progress",
+--             source = "vim.lsp",
+--             title = value.title,
+--             status = value.kind ~= "end" and "running" or "success",
+--             percent = value.percentage,
+--         })
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("vstegen_lsp_attach", {}),
